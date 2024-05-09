7977b6948be388cf4774fd336f320d275a9b8c9a
tungb12ok <99113840+tungb12ok@users.noreply.github.com>
2024-05-09 17:23:23 +0700

    Update DB

--- BE_ShopingCart/DataAccess/Migrations/20240509080946_InitialCreate.Designer.cs
+++ BE_ShopingCart/DataAccess/Migrations/20240509080946_InitialCreate.Designer.cs
@@ -4,17 +4,18 @@ using DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Microsoft.Extensions.Configuration;
 
 #nullable disable
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(FPTShopContext))]
-    partial class FPTShopContextModelSnapshot : ModelSnapshot
+    [Migration("20240509080946_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
